4cf6428c41f185273107dfa052f1eb710b9fcc0c
Dor Shtaif <dorshtaif@gmail.com>
2023-08-18 21:47:30 +0300

    meaningless change

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,5 @@ export {
 
 // TODO: Should implement such that when an instance is ended (or all has all its active iterators closed), it can always be reinitialized by just consuming it again, similarly with RxJS observables?
 // TODO: Research setting up UMD builds (see Igal's packages)
+
+//
